9f18659b089e8e9728b03485a087358d7be8882f
rzkynaufaal <rzkynaufaal746@gmail.com>
2024-11-12 11:32:18 +0300

    last commit 2

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Complaint;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class FrontController extends Controller
 {
